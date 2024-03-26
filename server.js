832c9857e277602af03990146985f8727ab81215
JayeshSabale <118163354+JayeshSabale@users.noreply.github.com>
2024-03-26 15:31:01 +0530

    home route added

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const app = express()
 app.use(express.json())
 app.use(routes)
 
+app.get('/', (req,res) => {
+  res.send('visit https://quotes-rest-api.onrender.com/api/quotes  to get all quotes')
+})
+
 app.listen(process.env.PORT, () => {
   console.log(
     `Server running on port number http://localhost:${process.env.PORT}`
